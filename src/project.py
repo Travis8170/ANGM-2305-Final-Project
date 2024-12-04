5eaf4e3c8cd146616ce3f880019b3fe4fd92718c
Travis Nguyen <travistri.nguyen@gmail.com>
2024-12-03 23:20:09 -0600

    snake trail created

--- src/project.py
+++ src/project.py
@@ -27,7 +27,7 @@ class Snake():
     def draw(self, surface):
         if self.dead:
             return
-        self.surface.set_alpha(self.alpha)
+        #self.surface.set_alpha(self.alpha)
         surface.blit(self.surface, self.pos)
         
 
@@ -42,14 +42,22 @@ class SnakeTrail():
     def update(self, dt):
         particle = Snake(self.pos, size=self.size, life=self.life)
         self.particles.insert(0, particle)
+        self._update_particles(dt)
         self._update_pos()
 
     def _update_pos(self):
         x, y = self.pos
         y += self.size
         self.pos = (x, y)
-
     #Insert method to find direction from button inputs
+
+    def _update_particles(self, dt):
+        for particle in self.particles:
+            particle.update(dt)
+
+    def draw(self, surface):
+        for particle in self.particles:
+            particle.draw(surface)
     
 
 def main():
@@ -59,7 +67,7 @@ def main():
     dt = 0
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution)
-    snake = Snake(life=1000)
+    snake = SnakeTrail((0, 0), size =20, life=3100)
     running = True
     while running:
         for event in pygame.event.get():
@@ -68,10 +76,9 @@ def main():
         snake.update(dt)
         black = pygame.Color(0, 0, 0)
         screen.fill(black)
-        snake.pos = (resolution[0]//2, resolution[1]//2)
         snake.draw(screen)
         pygame.display.flip()
-        dt = clock.tick()
+        dt = clock.tick(1)
     pygame.quit()
 
 
